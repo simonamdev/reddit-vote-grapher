c179c41d795161205ed8ab22975167c9b615bfb3
Simon AM <simon@agius-muscat.net>
2016-05-15 00:04:56 +0200

    Completed graph visualisation

---
+++ static/draw_graph.js
@@ -0,0 +1,133 @@
+function drawGraph(plotData) {
+	var score = [];
+	var up = [];
+	var down = [];
+	var ratio = [];
+	plotData.forEach(function(item, index) {
+		score.push({x: item[0], y: item[1]});
+		up.push({x: item[0], y: item[2]});
+		down.push({x: item[0], y: item[3]});
+		ratio.push({x: item[0], y: item[4]});
+	});
+	drawAll(score, up, down, ratio);
+	drawScore(score);
+	drawUp(up);
+	drawDown(down);
+	drawRatio(ratio);
+}
+
+function drawAll(dataScore, dataUp, dataDown, dataRatio) {
+	var container = document.getElementById('visualization-all');
+	var groups = new vis.DataSet();
+
+	groups.add({
+    id: 0,
+    content: 'score',
+	});
+
+	groups.add({
+    id: 1,
+    content: 'up',
+	});
+
+	groups.add({
+    id: 2,
+    content: 'down',
+	});
+
+	groups.add({
+    id: 3,
+    content: 'ratio',
+	});
+
+	items = [];
+
+	dataScore.forEach(function(item, index) {
+		items.push({x: item.x, y: item.y, group: 0});
+	});
+
+	dataUp.forEach(function(item, index) {
+		items.push({x: item.x, y: item.y, group: 1});
+	});
+
+	dataDown.forEach(function(item, index) {
+		items.push({x: item.x, y: item.y, group: 2});
+	});
+
+	dataRatio.forEach(function(item, index) {
+		items.push({x: item.x, y: item.y, group: 3});
+	});
+
+	var dataset = new vis.DataSet(items);
+	var options = {
+		legend: true,
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, groups, options);
+}
+
+function drawUp(dataUp) {
+	var container = document.getElementById('visualization-up');
+	var dataset = new vis.DataSet(dataUp);
+	var options = {
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, options);
+}
+
+function drawScore(dataScore) {
+	var container = document.getElementById('visualization-score');
+	var dataset = new vis.DataSet(dataScore);
+	var options = {
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, options);
+}
+
+function drawUp(dataUp) {
+	var container = document.getElementById('visualization-up');
+	var dataset = new vis.DataSet(dataUp);
+	var options = {
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, options);
+}
+
+function drawDown(dataDown) {
+	var container = document.getElementById('visualization-down');
+	var dataset = new vis.DataSet(dataDown);
+	var options = {
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, options);
+}
+
+function drawRatio(dataRatio) {
+	var container = document.getElementById('visualization-ratio');
+	var dataset = new vis.DataSet(dataRatio);
+	var options = {
+		shaded: true,
+		drawPoints: false
+	};
+	var graph2d = new vis.Graph2d(container, dataset, options);
+}
+
+function testVariables(plotData) {
+	var score = [];
+	var up = [];
+	var down = [];
+	var ratio = [];
+	plotData.forEach(function(item, index) {
+		score.push({x: item[0], y: item[1]});
+		up.push({x: item[0], y: item[2]});
+		down.push({x: item[0], y: item[3]});
+		ratio.push({x: item[0], y: item[4]});
+	});
+	console.log(score[score.length - 1].y);
+}
+
--- visualise.py
+++ visualise.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 file_data_list = []
 
 
+def convert_to_timestamp(epoch_time='0'):
+    return datetime.datetime.fromtimestamp(int(float(epoch_time))).strftime('%Y-%m-%dT%H:%M:%S')
+
+
 def cache_file_data():
     global file_data_list
     files_dir = os.path.join(os.getcwd(), 'data')
@@ -14,13 +18,14 @@ def cache_file_data():
     for file in csv_files:
         file_data = dict()
         file_data['name'] = file.replace('.csv', '').replace('_complete', '')
+        file_data['link'] = 'http://redd.it/' + file_data['name']
         file_data['path'] = os.path.join(files_dir, file)
         file_data['complete'] = True if 'complete' in file else False
         with open(file_data['path'], mode='r') as csvfile:
             csvreader = csv.reader(csvfile)
             data_list = list(csvreader)
             file_data['age'] = round((float(data_list[-1][0]) - float(data_list[0][0])) / (60 * 60), 2)
-            file_data['last_read'] = datetime.datetime.fromtimestamp(int(float(data_list[-1][0]))).strftime('%d/%m/%Y @ %H:%M:%S')
+            file_data['last_read'] = convert_to_timestamp(data_list[-1][0])
         file_data_list.append(file_data)
 
 
@@ -38,9 +43,27 @@ def refresh_data():
 
 @app.route('/graph/<name>')
 def graph(name):
-    return name
+    global file_data_list
+    for file in file_data_list:
+        if file['name'] == name:
+            with open(file['path'], mode='r') as csvfile:
+                csvreader = csv.reader(csvfile)
+                data_list = list(csvreader)
+            # convert all the epoch times to timestamps for graphing
+            data_list = [
+                [
+                    convert_to_timestamp(data[0]),
+                    data[1],
+                    data[2],
+                    data[3],
+                    data[4]
+                ] for data in data_list
+            ]
+            file['data'] = data_list
+            return render_template('graph.html', data=file)
 
 
 if __name__ == '__main__':
+    cache_file_data()
     app.run(host='127.0.0.1', port=5000, debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=False)
