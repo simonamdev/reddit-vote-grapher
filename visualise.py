c888616b7bc994cd1071bf31a25c57483a1daeaf
Simon AM <simon@agius-muscat.net>
2016-05-14 21:13:58 +0200

    List files in browser

--- visualise.py
+++ visualise.py
@@ -4,19 +4,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def return_file_list():
+def return_file_data():
     files_dir = os.path.join(os.getcwd(), 'data')
     csv_files = os.listdir(files_dir)
-    print('Found {} CSV files'.format(len(csv_files)))
-    return csv_files
+    file_data_list = []
+    for file in csv_files:
+        file_data = dict()
+        file_data['name'] = file.replace('.csv', '').replace('_complete', '')
+        file_data['path'] = os.path.join(files_dir, file)
+        file_data['complete'] = True if 'complete' in file else False
+        file_data_list.append(file_data)
+    return file_data_list
 
 
 """
-file_data = dict()
-csv_path = os.path.join(os.getcwd(), 'data', file)
-file_data['name'] = file
-file_data['path'] = csv_path
-file_data['rows'] = []
 with open(csv_path, mode='r', newline='') as csvfile:
     csvreader = csv.reader(csvfile)
     for row in csvreader:
@@ -29,7 +30,7 @@ file_data_list.append(file_data)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', file_data=return_file_data())
 
 
 @app.route('/refresh')
